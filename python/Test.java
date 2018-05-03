3f46965b32632469e801e8c398849450714b28f7
Mch233 <onemx2@gmail.com>
2018-05-03 21:01:00 +0800

    z

---
+++ python/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String[] args) {
+		int a = 3;
+		if (a > 5)
+				{
+			a = 5;
+			System.out.println(a);}
+		else 
+			System.out.println("hello world");
+	}
+}
---
+++ python/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main( )
+{
+	int a = 3;
+	if (a > 5){
+		a = 5;
+		std::cout << a <<std::endl;}
+	else 
+		std::cout <<"hello world"<<std::endl;
+}
